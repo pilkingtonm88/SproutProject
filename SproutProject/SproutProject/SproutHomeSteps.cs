52413dfea7cf06a3977b76f562313f88534f6fde
Mark <pilkingtonm88@gmail.com>
2018-05-04 16:05:47 -0400

    Add Class Library

---
+++ SproutProject/SproutProject/SproutHomeSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SproutProject
+{
+    [Binding]
+    public class SproutHomeSteps
+    {
+        [Given(@"I have entered the Sprout URL domain")]
+        public void GivenIHaveEnteredTheSproutURLDomain()
+        {
+            
+        }
+        
+        [Given(@"I have initiated the HTTP request")]
+        public void GivenIHaveInitiatedTheHTTPRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the page loads")]
+        public void WhenThePageLoads()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Sprout Home page should load")]
+        public void ThenTheSproutHomePageShouldLoad()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
